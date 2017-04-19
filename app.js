4c9f08c07a4e68ef12ba18700df969262d3069d3
Jairo <jairoportela@vah.com.br>
2017-04-19 18:11:18 -0300

    Simple logic

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict'
 import Victor from 'victor';
 
 function Entity(inputs) {
@@ -10,18 +11,56 @@ function Entity(inputs) {
   this.arg4 = +inputs[7];
 }
 
+function Ship(inputs) {
+  Entity.call(this, inputs);
+}
+
+Ship.prototype.move = function(barrels) {
+  let closest, distanceSq;
+  let compareBarrel = function(barrel) {
+    let currDistanceSq = barrel.pos.distanceSq(this.pos);
+    if(!closest || currDistanceSq > distanceSq) {
+      closest = barrel;
+      distanceSq = currDistanceSq;
+    }
+  };
+  compareBarrel = compareBarrel.bind(this);
+  barrels.forEach(compareBarrel);
+  if (closest) {
+    print(`MOVE ${closest.pos.x} ${closest.pos.y}`);
+  } else {
+    print('WAIT');
+  }
+
+
+}
+
+function isMyShip(entity) {
+  return entity.type === 'SHIP' && entity.arg4 === 1;
+}
+
+function isBarrel(entity) {
+  return entity.type === 'BARREL';
+}
+
 function init() {
   while(true) {
-    var myShipCount = +readline();
-    var entityCount = +readline();
-    for (var i = 0; i < entityCount; i++) {
-      var inputs = readline().split(' ');
-      entities.push(new Entity(inputs));
+    let entities = [];
+    let myShips = [];
+    let barrels = [];
+    let myShipCount = +readline();
+    let entityCount = +readline();
+    for (let i = 0; i < entityCount; i++) {
+      let inputs = readline().split(' ');
+      let entity = new Entity(inputs);
+      if (isMyShip(entity)) {
+        myShips.push(new Ship(inputs));
+      } else if (isBarrel(entity)) {
+        barrels.push(entity);
+      }
     }
 
-    for (var i = 0; i < myShipCount; i++) {
-      print('MOVE 11 10');
-    }
+    myShips.forEach((s) => s.move(barrels));
   }
 }
 
